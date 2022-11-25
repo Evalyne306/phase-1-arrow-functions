1e80a7acdfc5063318a73fc78c475fcbe7a64680
Evalyne306 <evalyne.wambui@student.moringaschool.com>
2022-11-25 16:46:50 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,36 @@
+
+
+ const divide = function() {
+    const divide = 2000 / 100
+    return divide
+ }
+ const square = function() {
+    let y =2
+    const square = y * y
+    return square
+ }
+ 
+ const add = (parameter1, parameter2) => parameter1 + parameter2;  
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+  
+  
